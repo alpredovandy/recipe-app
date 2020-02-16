414cd26c982d9838b2d6343d61823763785ae3b7
root <root@localhost.localdomain>
2020-02-16 22:54:30 +0700

    Updates

---
+++ precache-manifest.e85f9134b2e84c4969a12d4a333538ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99187f7636aee15ccc7224c5109fb6f5",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "1339e9ea9d85c20b4e5e",
+    "url": "/recipe-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d4a79eecf0c22eee67dc",
+    "url": "/recipe-app/static/js/2.fe2bce52.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/recipe-app/static/js/2.fe2bce52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1339e9ea9d85c20b4e5e",
+    "url": "/recipe-app/static/js/main.6aa714e6.chunk.js"
+  },
+  {
+    "revision": "148e400df5fd6abd3a3f",
+    "url": "/recipe-app/static/js/runtime-main.5fe0ecde.js"
+  }
+]);
